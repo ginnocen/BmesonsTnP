791d4c4d817fe0f6ce517057ff069cefee693754
ginnocen <ginnocen@cern.ch>
2014-10-19 14:23:32 +0200

    new version

--- Display/doAll.sh
+++ Display/doAll.sh
@@ -1,6 +1,9 @@
 rm an.log
-rm Results/foutputTrigger.root
-rm Plots/canvasTrg.pdf
+rm Results/*.root
+rm Plots/*.pdf
+
+
+#-----------------------ConvertIntoRootFiles-------------------
 
 time root -b > an.log 2>&1 <<EOI
 .x ConvertIntoRootFiles.C("Trg")
@@ -8,16 +11,19 @@ time root -b > an.log 2>&1 <<EOI
 EOI
 
 time root -b > an.log 2>&1 <<EOI
-.x CompareMCData.C("Trg")
+.x ConvertIntoRootFiles.C("Trk")
 .q
 EOI
 
-rm an.log
-rm Results/foutputTracking.root
-rm Plots/canvasTrk.pdf
+time root -b > an.log 2>&1 <<EOI
+.x ConvertIntoRootFiles.C("MuonID")
+.q
+EOI
+
+#-----------------------CompareMCData-------------------
 
 time root -b > an.log 2>&1 <<EOI
-.x ConvertIntoRootFiles.C("Trk")
+.x CompareMCData.C("Trg")
 .q
 EOI
 
@@ -26,17 +32,42 @@ time root -b > an.log 2>&1 <<EOI
 .q
 EOI
 
-
-rm an.log
-rm Results/foutputMuonID.root
-rm Plots/canvasTrg.pdf
-
-time root -b > an.log 2>&1 <<EOI
-.x ConvertIntoRootFiles.C("MuonID")
-.q
-EOI
-
 time root -b > an.log 2>&1 <<EOI
 .x CompareMCData.C("MuonID")
 .q
 EOI
+
+#-----------------------CompareMCData-------------------
+
+
+
+time root -b > an.log 2>&1 <<EOI
+.x CompareMethods.C("Trg","Pt","MC")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x CompareMethods.C("Trk","Pt","MC")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x CompareMethods.C("MuID","Pt","MC")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x CompareMethods.C("Trg","Pt","Data")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x CompareMethods.C("Trk","Pt","Data")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x CompareMethods.C("MuID","Pt","Data")
+.q
+EOI
+
