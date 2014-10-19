4ff42ec44100e25c992fe9adfcf4a83061e057ec
ginnocen <ginnocen@cern.ch>
2014-10-19 19:04:42 +0200

    add fits

--- Display/doAll.sh
+++ Display/doAll.sh
@@ -71,3 +71,40 @@ time root -b > an.log 2>&1 <<EOI
 .q
 EOI
 
+
+#-----------------------save plots-------------------
+
+
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("Data","Trigger")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("Data","Tracking")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("Data","MuonID")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("MC","Trigger")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("MC","Tracking")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("MC","MuonID")
+.q
+EOI
+
+
+
