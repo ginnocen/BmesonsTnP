8fac936e5e80ab357e4fd76ea37fb2e3af6b830b
ginnocen <ginnocen@cern.ch>
2014-10-19 14:41:50 +0200

    modifications to add the tag selection in the macro

--- Analysis/fitTrigger_pPb.py
+++ Analysis/fitTrigger_pPb.py
@@ -8,10 +8,10 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
 process.TnP_Trigger = cms.EDAnalyzer("TagProbeFitTreeAnalyzer", 
     ## Input, output 
-    #InputFileNames = cms.vstring("file:../Inputs/tnpJPsi_pPb_MC_17October_v1.root"), 
-    #OutputFileName = cms.string("../ResultsFit/outputTriggerMC.root"),
-    InputFileNames = cms.vstring("file:../Inputs/tnpJPsi_pPb_Data_17October_v2.root"), 
-    OutputFileName = cms.string("../ResultsFit/outputTriggerData.root"),
+    InputFileNames = cms.vstring("file:../Inputs/tnpJPsi_MC_19October_testtriggerHyunchul_testtag_v1.root"), 
+    OutputFileName = cms.string("../ResultsFit/outputTriggerMC_19October_testtriggerHyunchul_testtag_v1.root"),
+    #InputFileNames = cms.vstring("file:../Inputs/tnpJPsi_pPb_Data_17October_v2.root"), 
+    #OutputFileName = cms.string("../ResultsFit/outputTriggerData.root"),
     InputTreeName = cms.string("fitter_tree"),
     InputDirectoryName = cms.string("tpTree"),
     ## Variables for binning
@@ -23,7 +23,10 @@ process.TnP_Trigger = cms.EDAnalyzer("TagProbeFitTreeAnalyzer",
     ),
     ## Flags you want to use to define numerator and possibly denominator
     Categories = cms.PSet(
-	PAMu3 = cms.vstring("HLT_PAMu3", "dummy[pass=1,fail=0]"),
+	PAMu3 = cms.vstring("PAMu3", "dummy[pass=1,fail=0]"),
+	PAMu3_standard = cms.vstring("PAMu3_standard", "dummy[pass=1,fail=0]"),
+	tag_PAMu3 = cms.vstring("tag_PAMu3", "dummy[pass=1,fail=0]"),
+	tag_PAMu3_standard = cms.vstring("tag_PAMu3_standard", "dummy[pass=1,fail=0]"),
 	Acc_JPsi= cms.vstring("Acc_JPsi", "dummy[pass=1,fail=0]"),	
 	TrackCuts= cms.vstring("TrackCuts", "dummy[pass=1,fail=0]"),
 	MuonID= cms.vstring("MuonID", "dummy[pass=1,fail=0]"),
@@ -33,12 +36,13 @@ process.TnP_Trigger = cms.EDAnalyzer("TagProbeFitTreeAnalyzer",
     Efficiencies = cms.PSet(
         Trg_pt = cms.PSet(
             UnbinnedVariables = cms.vstring("mass"),
-            EfficiencyCategoryAndState = cms.vstring("PAMu3", "pass"), ## Numerator definition
+            EfficiencyCategoryAndState = cms.vstring("PAMu3_standard", "pass"), ## Numerator definition
             BinnedVariables = cms.PSet(
                 ## Binning in continuous variables
                 eta = cms.vdouble(-2.4, 2.4),
                 pt = cms.vdouble(1.5,2.5,3.,3.5,4.,4.5,6.,9.,20.,30),
                 ## flags and conditions required at the denominator, 
+                tag_PAMu3_standard = cms.vstring("pass"), 
                 Acc_JPsi = cms.vstring("pass"), 
                 TrackCuts = cms.vstring("pass"), 
                 MuonID = cms.vstring("pass"), 
