e7ff5e8aaea8685def4f8512780c2c8ea2385770
ginnocen <ginnocen@cern.ch>
2014-10-25 01:25:45 +0200

    new eta bin and pt bin

--- Display/doAll.sh
+++ Display/doAll.sh
@@ -1,6 +1,7 @@
 rm an.log
 rm Results/*.root
 rm Plots/*.pdf
+rm PlotsFit/*.pdf
 
 
 #-----------------------ConvertIntoRootFiles-------------------
@@ -77,32 +78,128 @@ EOI
 
 
 time root -b > an.log 2>&1 <<EOI
-.x SaveFitPlots.C("Data","Trigger")
+.x SaveFitPlots.C("Data","Trigger","all")
 .q
 EOI
 
 time root -b > an.log 2>&1 <<EOI
-.x SaveFitPlots.C("Data","Tracking")
+.x SaveFitPlots.C("Data","Tracking","all")
 .q
 EOI
 
 time root -b > an.log 2>&1 <<EOI
-.x SaveFitPlots.C("Data","MuonID")
+.x SaveFitPlots.C("Data","MuonID","all")
 .q
 EOI
 
 time root -b > an.log 2>&1 <<EOI
-.x SaveFitPlots.C("MC","Trigger")
+.x SaveFitPlots.C("MC","Trigger","all")
 .q
 EOI
 
 time root -b > an.log 2>&1 <<EOI
-.x SaveFitPlots.C("MC","Tracking")
+.x SaveFitPlots.C("MC","Tracking","all")
 .q
 EOI
 
 time root -b > an.log 2>&1 <<EOI
-.x SaveFitPlots.C("MC","MuonID")
+.x SaveFitPlots.C("MC","MuonID","all")
+.q
+EOI
+
+#---------
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("Data","Trigger","etabin0")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("Data","Tracking","etabin0")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("Data","MuonID","etabin0")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("MC","Trigger","etabin0")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("MC","Tracking","etabin0")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("MC","MuonID","etabin0")
+.q
+EOI
+
+#---------
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("Data","Trigger","etabin1")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("Data","Tracking","etabin1")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("Data","MuonID","etabin1")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("MC","Trigger","etabin1")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("MC","Tracking","etabin1")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("MC","MuonID","etabin1")
+.q
+EOI
+
+#---------
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("Data","Trigger","etabin2")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("Data","Tracking","etabin2")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("Data","MuonID","etabin2")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("MC","Trigger","etabin2")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("MC","Tracking","etabin2")
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SaveFitPlots.C("MC","MuonID","etabin2")
 .q
 EOI
 
