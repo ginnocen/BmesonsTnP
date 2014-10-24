17d33444d31dacd8132216ffb2b09dceb37a6601
ginnocen <ginnocen@cern.ch>
2014-10-25 01:25:19 +0200

    new eta bin and pt bin

--- Display/SaveFitPlots.C
+++ Display/SaveFitPlots.C
@@ -1,19 +1,19 @@
-void SaveFitPlots(TString data="Data",TString type="Trigger"){
+void SaveFitPlots(TString data="MC",TString type="MuonID",TString etabin="all"){
 
   TFile *file = new TFile(Form("../ResultsFit/output%s%s.root",type.Data(),data.Data()));
   
   TDirectory *dir;  
   TCanvas *canvas;
   
-  for (int ipt=0;ipt<9;ipt++){
+  for (int ipt=0;ipt<5;ipt++){
   
   
-  if(type=="Trigger")dir=(TDirectory*)file->GetDirectory(Form("tpTree/Trg_pt/eta_bin0__pt_bin%d__Acc_JPsi_pass__MuonID_pass__TrackCuts_pass__tag_PAMu3_standard_pass__cbGaussPlusExpo",ipt));
-  if(type=="MuonID") dir=(TDirectory*)file->GetDirectory(Form("tpTree/MuID_pt/eta_bin0__pt_bin%d__Acc_JPsi_pass__TrackCuts_pass__tag_PAMu3_standard_pass__cbGaussPlusExpo",ipt));
-  if(type=="Tracking") dir=(TDirectory*)file->GetDirectory(Form("tpTreeSta/Trk_pt/eta_bin0__pt_bin%d__Acc_JPsi_pass__tag_PAMu3_standard_pass__twoGaussPlusPoly1",ipt));
+  if(type=="Trigger")dir=(TDirectory*)file->GetDirectory(Form("tpTree/Trg_pt_%s/eta_bin0__pt_bin%d__Acc_JPsi_pass__MuonID_pass__TrackCuts_pass__tag_PAMu3_standard_pass__cbGaussPlusExpo",etabin.Data(),ipt));
+  if(type=="MuonID") dir=(TDirectory*)file->GetDirectory(Form("tpTree/MuID_pt_%s/eta_bin0__pt_bin%d__Acc_JPsi_pass__TrackCuts_pass__tag_PAMu3_standard_pass__cbGaussPlusExpo",etabin.Data(),ipt));
+  if(type=="Tracking") dir=(TDirectory*)file->GetDirectory(Form("tpTreeSta/Trk_pt_%s/eta_bin0__pt_bin%d__Acc_JPsi_pass__tag_PAMu3_standard_pass__twoGaussPlusPoly1",etabin.Data(),ipt));
 
    canvas=(TCanvas*)dir->Get("fit_canvas");
 
-   canvas->SaveAs(Form("PlotsFit/canvas%s_%s_PtBin%d.pdf",type.Data(),data.Data(),ipt));
+   canvas->SaveAs(Form("PlotsFit/canvas%s_%s_PtBin%d_%s.pdf",type.Data(),data.Data(),ipt,etabin.Data()));
   }
 }
